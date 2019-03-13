6dd986cba55f6a29ac21805532264ed0745430d3
Gyimesi Péter <pgyimesi@inf.u-szeged.hu>
2019-03-13 10:24:46 +0100

    b35

--- btest/src/Main3.java
+++ btest/src/Main3.java
@@ -7,6 +7,7 @@ public class Main3 {
 	public void write() {
         System.out.println("Hello, World");
         System.out.println("Hello, World");
+        System.out.println("Hello, World");
 	}
 
 }
